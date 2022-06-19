e745a044e1c68d08c39d748ba130270a7cbefb2e
丁伟东 <Weydon-Ding@outlook.com>
2022-06-19 18:39:49 +0800

    feat: add "installer" & "uninstaller"

---
+++ Installer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using SharpShell.Helpers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFileName = @".\";
+            string destFileName = Environment.Is64BitOperatingSystem ?
+                @"C:\Program Files\MyGit" :
+                @"C:\Program Files (x86)\MyGit";
+            string[] copyFiles = new string[5];
+            copyFiles[0] = "MyGit.dll";
+            copyFiles[1] = "SharpShell.dll";
+            copyFiles[2] = "SharpShell.xml";
+            copyFiles[3] = "uninstall.exe";
+            copyFiles[4] = "arrow_undo.png";
+
+            System.IO.Directory.CreateDirectory(destFileName);
+            foreach (var copyFile in copyFiles)
+            {
+                System.IO.File.Copy(System.IO.Path.Combine(sourceFileName, copyFile),
+                    System.IO.Path.Combine(destFileName, copyFile), true);
+            }
+
+            string MyGit = System.IO.Path.Combine(destFileName, "MyGit.dll");
+            var regasm = new RegAsm();
+            var success = Environment.Is64BitOperatingSystem ?
+                regasm.Register64(MyGit, true) :
+                regasm.Register32(MyGit, true);
+
+
+            if (success)
+            {
+                MessageBox.Show("successfully");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+        }
+    }
+}
---
+++ Uninstaller/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using SharpShell.Diagnostics;
+using SharpShell.Helpers;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string installPath = Environment.Is64BitOperatingSystem ?
+                @"C:\Program Files\MyGit\MyGit.dll" :
+                @"C:\Program Files (x86)\MyGit\MyGit.dll";
+            var regasm = new RegAsm();
+            var success = Environment.Is64BitOperatingSystem ?
+                regasm.Unregister64(installPath) :
+                regasm.Unregister64(installPath);
+            if (success)
+            {
+                MessageBox.Show("successfully");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+            ExplorerManager.RestartExplorer();
+        }
+    }
+}
